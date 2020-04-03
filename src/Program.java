d2137eaa699304fbb2f7452e6ea4cade6bedd45c
Asan86 <asanturdukulov86@gmail.com>
2020-04-04 00:36:01 +0600

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+
+
+        public static void main(String[] args) {
+
+            Person tom = new Person("Tom", "qwerty");
+            tom.displayPerson();
+            tom.account.displayAccount();
+        }
+    }
+
